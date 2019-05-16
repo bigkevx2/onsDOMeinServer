a03f0fa73bcf4cf5244fb57e47580fa3522cade8
Tim H <t.hilker@student.fontys.nl>
2019-05-16 20:57:01 +0200

    Showing Anna around.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Just checking out your server!
+        // Showing Anna around GitHub
         Server server = new Server();
     }
 }
